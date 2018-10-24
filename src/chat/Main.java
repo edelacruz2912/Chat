3ec43de4841d8228c47f8515658fe9d1b0aa9270
Eduardo De La Cruz <eduardodelacruz2912@gmail.com>
2018-10-24 14:35:47 -0400

    test code

--- src/chat/Main.java
+++ src/chat/Main.java
@@ -1,10 +1,30 @@
 package chat;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("CHAT");
+		StackPane layout = new StackPane();
+		
+		Scene scene = new Scene(layout,500,500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 
 }
