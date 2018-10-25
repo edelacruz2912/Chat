519485d2c59fc43f6f9470a5013e54e07354661a
Eduardo De La Cruz <eduardodelacruz2912@gmail.com>
2018-10-25 16:35:21 -0400

    redesignning the window object

--- src/chat/Window.java
+++ src/chat/Window.java
@@ -15,72 +15,27 @@ import javafx.scene.layout.VBox;
 
 public class Window extends BorderPane implements EventHandler<ActionEvent>{
 
-	//Send Button 
-	Button sendBtn;
-	//Add Button 
-	Button addButton;
 	
-	//Top are for the chat
-	TextArea chatTopAreaTextArea;
+	//For the Top regions
+	VBox topRegionVBoxLayout;	
+	TextArea topRegionTextA;	
 	
-	//Top are for the chat
-	TextArea chatBottonAreaTextArea;
+	//For the Center regions
+	VBox centerRegionVBoxLayout;	
+	TextArea centerRegionTextA;	
 	
-	//Label for the Ip and port Number
-	Label IPandPort;	
+	//For the Left regions
+	VBox leftRegionVBoxLayout;	
+	TextArea leftRegionTextA;
+	Button connect;
+	Button exit;	
 	
-	//vertical Box for the chatTopArea
-	VBox TopWindowChatBoxLayout;
-	
-	//Horizontal Box for the chatTopArea
-	HBox horizontalAddBoxLayout;
-	
-	//TextArea for IpNumber and PortNumber
-	TextArea ipNumberInput;
-	TextArea portNumBerInput;	
-	
-	
-	String IpSource = "000.000.0.000";
-	String portNumber = "00000";
+	//For the Bottom regions	
+	Button send;
 	
 	public Window()
 	{
-		//chat Button
-		sendBtn = new Button("send");
-		//add button 
-		addButton = new Button("add");		
-		//setting the listener to buttons.
-		sendBtn.setOnAction(this); 
-		addButton.setOnAction(this);
 		
-		//Label for the IP and port Number
-		IPandPort = new Label("Ip: "+ IpSource +  " Port: " + portNumber);
-		
-		//for the topChatArea
-		TopWindowChatBoxLayout = new VBox(8); 		
-		//Top area for the chat
-		chatTopAreaTextArea = new TextArea();
-		chatTopAreaTextArea.setMaxWidth(400);
-		//chatTopArea.setMaxHeight(450);
-		chatTopAreaTextArea.setEditable(false);
-		
-		//Bottom area for the chat
-		chatBottonAreaTextArea = new TextArea();
-		chatBottonAreaTextArea.setMaxHeight(90);
-		chatBottonAreaTextArea.setEditable(true);
-		
-		
-		// Adding to the vBox and alignment to the layout Vbox .
-		TopWindowChatBoxLayout.setAlignment(Pos.CENTER);
-		
-		TopWindowChatBoxLayout.getChildren().addAll(IPandPort,chatTopAreaTextArea);
-		
-		//--------Setting components to BorderRegions---------
-		//setting component of the textArea to Top
-		this.setTop(TopWindowChatBoxLayout);
-		
-		//setting component of the textArea to Center
-		this.setCenter(chatBottonAreaTextArea);
 		
 		
 		 
@@ -92,16 +47,16 @@ public class Window extends BorderPane implements EventHandler<ActionEvent>{
 	public void handle(ActionEvent event) {
 		// TODO Auto-generated method stub
 		
-		if(event.getSource() == sendBtn)
+	/*if(event.getSource() == )
 		{
 			System.out.println("button Works");
 		}
 		
-		else if(event.getSource() == addButton)
+		else if(event.getSource() == )
 		{
 			System.out.println("add button works");
 		}
-		
+	*/	
 		
 		
 		
