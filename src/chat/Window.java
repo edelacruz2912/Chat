768e162bece552f056dd85e927cd26136de5e747
Eduardo De La Cruz <eduardodelacruz2912@gmail.com>
2018-10-25 19:33:27 -0400

    create Chat GUI

--- src/chat/Window.java
+++ src/chat/Window.java
@@ -7,6 +7,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
@@ -16,29 +17,67 @@ import javafx.scene.layout.VBox;
 public class Window extends BorderPane implements EventHandler<ActionEvent>{
 
 	
-	//For the Top regions
-	VBox topRegionVBoxLayout;	
-	TextArea topRegionTextA;	
+	//For the Top part of chat in center region
+	private VBox centerContainerVLayout;	
+	private TextArea topPartOfChatTextA;	
 	
-	//For the Center regions
-	VBox centerRegionVBoxLayout;	
-	TextArea centerRegionTextA;	
+	//For the bottom part of chat in center region
+	//private VBox centerRegionVBoxLayout;	
+	private TextArea bottomPartOfChatTextA;
 	
-	//For the Left regions
-	VBox leftRegionVBoxLayout;	
-	TextArea leftRegionTextA;
-	Button connect;
-	Button exit;	
+	//Component button For center regions	
+	private Button sendBtn;
 	
-	//For the Bottom regions	
-	Button send;
+	//Components For the Left regions to connect to other computer
+	private VBox leftRegionVBoxLayout;	
+	private TextField IPnumberTextA;
+	private TextField PortNumberTextA;
+	private Button connectBtn;
+	private Button exitBtn;	
+	
+	
 	
 	public Window()
 	{
+		//For the Center regions
+		centerContainerVLayout = new VBox();
+		centerContainerVLayout.setPadding(new Insets(10));
+		centerContainerVLayout.setSpacing(8);
+		
+		//--------COMPONENTS FOR CENTER-----------
+		
+		//top part of the Chat on the center Region
+		topPartOfChatTextA = new TextArea();
+		topPartOfChatTextA.setEditable(false);
+		//Bottom part of the chat on the center Region
+		bottomPartOfChatTextA = new TextArea();
+		bottomPartOfChatTextA.setEditable(true);
+		//sendBtn
+		sendBtn = new Button("Send");
+		//adding all components to the VBox
+		centerContainerVLayout.getChildren().addAll(topPartOfChatTextA,bottomPartOfChatTextA,sendBtn);
+		
+		
+		//FOR THE LEFT REGION
+		leftRegionVBoxLayout = new VBox();
+		centerContainerVLayout.setPadding(new Insets(5));
+		centerContainerVLayout.setSpacing(2);
+		
+		//---COMPONETS FOR LEFT REGION----
+		IPnumberTextA = new TextField();
+		PortNumberTextA = new TextField();
+		
+		connectBtn = new Button("Connect");
+		exitBtn = new Button("Exit");
+		
+		//adding components to the left Region to leftRegionVBoxLayout
+		leftRegionVBoxLayout.getChildren().addAll(IPnumberTextA,PortNumberTextA,connectBtn,exitBtn);
 		
 		
 		
-		 
+		//Setting components for each region in the BorderPane 
+		this.setCenter(centerContainerVLayout);//adding all the components to the center regions
+		this.setLeft(leftRegionVBoxLayout);//adding all the components for left region inside the VBox
 		
 	}
 	
@@ -47,16 +86,19 @@ public class Window extends BorderPane implements EventHandler<ActionEvent>{
 	public void handle(ActionEvent event) {
 		// TODO Auto-generated method stub
 		
-	/*if(event.getSource() == )
+		if(event.getSource() == sendBtn)
 		{
-			System.out.println("button Works");
+			System.out.println("sendbutton Works");
 		}
 		
-		else if(event.getSource() == )
+		else if(event.getSource() == connectBtn)
 		{
-			System.out.println("add button works");
+			System.out.println("connectBtn button works");
 		}
-	*/	
+		else if(event.getSource() == exitBtn)
+		{
+			System.out.println("exitBtn button works");
+		}
 		
 		
 		
